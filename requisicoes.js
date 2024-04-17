e594361f349bf1338163d13524e071775d6ced70
joao <juniorlacerdasilva@gmail.com>
2024-04-17 13:44:31 -0300

    criado arquivo js contato
    no arquivo requisicoes foi adicionado função POST para enviar os dados para API

--- requisicoes.js
+++ requisicoes.js
@@ -7,3 +7,24 @@ async function requestGet(endpoint, id){
 
     return responseJson
 }
+
+async function requestPost(){
+
+    const body = {
+        nome: 'joao',
+        sobrenome: 'lacerda',
+        email: 'joaoalacerda@gmail.com',
+        mensagem: 'testando o formulário'
+    }
+
+    const url = "https://api-aula.up.railway.app/generica"
+
+    const returnFetch = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify(body),
+        headers: {"Content-type": "application/json"}
+    })
+
+    const fetchJson = returnFetch.json()
+    console.log(fetchJson)
+}
