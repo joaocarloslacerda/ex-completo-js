5e758a9a454323f76f08296ef7e93aac3d49beab
joao <juniorlacerdasilva@gmail.com>
2024-04-23 13:21:17 -0300

    Criados arquivos indexMediaQuery e styleContatoMediaQuery

--- index.js
+++ index.js
@@ -10,14 +10,13 @@ function calculadora(){
 }
 
 
-async function  pokedex(){
+async function pokedex(){
 
     endpoint = "https://pokeapi.co/api/v2/pokemon"
 
     const idPokemon = Number(document.getElementById("idPokemon").value)
 
     const pokemon = await requestGet(endpoint, idPokemon)
-    console.log(pokemon)
 
     const nomePokemon = document.getElementById("nome")
     const imagemPokemon = document.getElementById("imgPokemon")
