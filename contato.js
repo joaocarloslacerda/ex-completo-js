0493cd5fbba6acdc31f38dc8fe12534dbc35fec2
joao <juniorlacerdasilva@gmail.com>
2024-04-29 00:27:34 -0300

    adicionada validação para identificar se algum campo está vazio

--- contato.js
+++ contato.js
@@ -5,6 +5,11 @@ async function enviarMensagem(){
     const email = document.getElementById("email").value
     const mensagem = document.getElementById("mensagem").value
 
+    const responseValidaFormulario = validaFormulario(nome, sobrenome, email, mensagem)
+    if(responseValidaFormulario == false){
+        return;
+    }
+
     const url = "https://api-aula.up.railway.app/generica"
 
     const body = {
@@ -15,12 +20,24 @@ async function enviarMensagem(){
     }
     const response = await requestPost(url, body)
 
-    validaEnvio(response)
+    validaRequest(response)
 }
 
-function validaEnvio(response){
+function validaFormulario(nome, sobrenome, email, mensagem){
 
-    const campoRetornoEnvio = document.getElementById("retornoEnvio")
+    const campoRetornoEnvio = document.getElementById("retorno")
+
+    if((nome.trim() === '') || (sobrenome.trim() === '') || (email.trim() === '') || (mensagem.trim() === '')){
+        campoRetornoEnvio.style.display = "block"
+        campoRetornoEnvio.style.backgroundColor = "#8B0000"
+        campoRetornoEnvio.innerText = "Formulário inválido!"
+        return false
+    }
+}
+
+function validaRequest(response){
+
+    const campoRetornoEnvio = document.getElementById("retorno")
 
     if(response.status == 200 || response.status == 201 || response.status == 204){
         campoRetornoEnvio.style.display = "block"
